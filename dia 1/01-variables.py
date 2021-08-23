f0317bd52e93320785b899861156675e43840491
rifflester <59460834+rifflester@users.noreply.github.com>
2021-08-23 17:28:56 -0500

    feat: agregue el archivo variables.py

---
+++ dia 1/01-variables.py
@@ -0,0 +1,20 @@
+# Esto es un comentario
+
+# variables de texto
+nombre = "Brandon"
+apellido = 'Ulloa'
+
+direcion = "calle o'higgins 125"
+
+texto = """yo tengo un miau
+    y es mi amigo porque es un gato baka
+    jaja xd"""
+
+texto2= '''yara
+me llamó noriega'''
+
+# variables numéricas
+año = 2021
+estatura = 1.92
+
+print(type(nombre))
